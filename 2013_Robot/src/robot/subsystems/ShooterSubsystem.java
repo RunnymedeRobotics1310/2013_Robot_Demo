8c4f5e76cf594a71bc9796417bd156a766080a70
Ryan Lounds <ryan.f.lounds@gmail.com>
2018-03-09 07:36:57 -0500

    Shooter adjustments

--- 2013_Robot/src/robot/subsystems/ShooterSubsystem.java
+++ 2013_Robot/src/robot/subsystems/ShooterSubsystem.java
@@ -86,7 +86,7 @@ public class ShooterSubsystem extends Subsystem {
     }
 
     public boolean aboveShootThreshold() {
-        return false ;
+        return true ;
     }
     
     public void updatePeriodic() {
